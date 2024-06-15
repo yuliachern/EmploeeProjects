d2960540581bbab9f784b486f4829840eb089057
yuliach <yuliachern9@gmail.com>
2024-06-15 11:42:13 +0200

    Added first and last name prompts

--- EmploeeProject/Program.cs
+++ EmploeeProject/Program.cs
@@ -1,11 +1,18 @@
 ﻿// Declare variables
-string name = string.Empty;
+using System.Xml.Linq;
+
+string Firstname = string.Empty;
+string Lastname = string.Empty;
 int retirementAge = 65;
 int age = 0;
 
 //Promptnthe user for input
-Console.WriteLine("Please, enter your name");
-name = Console.ReadLine();
+Console.WriteLine("Please, enter your Firstname");
+Firstname = Console.ReadLine();
+
+Console.WriteLine("Please, enter your Lastname");
+Lastname = Console.ReadLine();
+
 Console.WriteLine("Please, enter yor age");
 age = Convert.ToInt32(Console.ReadLine());
 
@@ -13,7 +20,7 @@ age = Convert.ToInt32(Console.ReadLine());
 int workingYearsRemaining = retirementAge - age;
 
 //Output the results to the user
-Console.WriteLine("Full name" + name);
+Console.WriteLine("Full name" + Firstname + Lastname);
 Console.WriteLine($"Age {age}");
 Console.WriteLine($"Working years remaining {workingYearsRemaining}");
 
