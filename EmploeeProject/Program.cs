ac2edadaf4520363ec65edb56cfb15406a07d02a
yuliach <yuliachern9@gmail.com>
2024-06-16 14:26:45 +0200

    added if eslse condition depends on gender and years.

--- EmploeeProject/Program.cs
+++ EmploeeProject/Program.cs
@@ -6,8 +6,10 @@ string Lastname = string.Empty;
 int retirementAge = 65;
 int age = 0;
 decimal salary = 0;
-char gender = char.MaxValue;
+char gender = char.MinValue;
 bool working = false;
+char genderM = 'm';
+char genderF = 'f';
 
 //Promptnthe user for input
 Console.WriteLine("Please, enter your Firstname");
@@ -16,7 +18,7 @@ Firstname = Console.ReadLine();
 Console.WriteLine("Please, enter your Lastname");
 Lastname = Console.ReadLine();
 
-Console.WriteLine("Please, enter yor age");
+Console.WriteLine("Please, enter your age");
 age = Convert.ToInt32(Console.ReadLine());
 
 Console.WriteLine("Please, enter your selary");
@@ -29,14 +31,48 @@ Console.WriteLine("Are you working? (true or false)");
 working = Convert.ToBoolean(Console.ReadLine());
 
 //Process the data
-int workingYearsRemaining = retirementAge - age;
+int workingYearsRemaining1 = retirementAge - age;
 
 //Output the results to the user
-Console.WriteLine("Full name" + Firstname + Lastname);
+Console.WriteLine($"Full name {Firstname} {Lastname}");
 Console.WriteLine($"Age {age}");
 Console.WriteLine($"Your gender is : {gender}");
 Console.WriteLine($"Your salary is : {salary}");
 Console.WriteLine($"You are emploed : {working}");
-Console.WriteLine($"Number of working years remaining : {workingYearsRemaining}");
 
+if (gender == 'm')
+{
+    if (age < 65)
+    {
+        int workingYearsRemaining = retirementAge - age;
+        Console.WriteLine($"You will retire in {workingYearsRemaining}");
+    }
+    else if (age == 65)
+    {
+        Console.WriteLine($"You are on pension now, becouse you are {age} year old");
+    }
+    else
+    {
+        int workingYearsRemaining = age - retirementAge;
+        Console.WriteLine($"You have been retired for {workingYearsRemaining} years");
+    }
+}
+else if (gender == 'f')
+{
+    if (age < 55)
+    {
+        int workingYearsRemaining = 55 - age;
+        Console.WriteLine($"You will retire in {workingYearsRemaining}");
+    }
+    else if (age == 55)
+    {
+        Console.WriteLine($"You are on pension now, becouse you are {age} year old");
+    }
+    else
+    {
+        int workingYearsRemaining = age - retirementAge;
+        Console.WriteLine($"You have been retired for {workingYearsRemaining} years");
+    }
+
+}
 
